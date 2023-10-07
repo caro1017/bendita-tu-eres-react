4c4759d21a697c940c73e33a410938bbf9094274
Carolina Uribe Botero <uribebotero.carolin@gmail.com>
2023-10-07 18:29:05 -0500

    ajuste imagenes

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,3 @@
-import { Images } from "../Images/Images";
 
 import { useState, useEffect } from "react";
 
@@ -48,7 +47,7 @@ export const Menu = () => {
       <nav className="nav-style nav-wrapper">
         {/* Logo */}
         <Link to="/" className="brand-logo">
-          <img src={Images.logo} alt="Logo" />
+          <img src="/src/assets/img/logo/LogoBendita.svg" alt="Logo" />
         </Link>
         {/* Menu hamburguesa */}
         <Link to="#" data-target="mobile-demo" className="sidenav-trigger left">
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -40,9 +40,9 @@ const HomePage = () => {
           </div>
         </div>
         <ImgHome
-          srcEscritorio="/public/header/img_home.png"
-          srcTablet="/public/header/img_home_tablet.png"
-          srcMovil= "/public/header/img_home_movil.png"
+          srcEscritorio="/src/assets/img/header/img_home.png"
+          srcTablet="/src/assets/img/header/img_home_tablet.png"
+          srcMovil= "/src/assets/img/header/img_home_movil.png"
         />
       </div>
     </>
